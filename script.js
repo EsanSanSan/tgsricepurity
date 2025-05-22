5a381ba788dea4f4d24da3c50c79784c61839782
EsanSanSan <86023925+EsanSanSan@users.noreply.github.com>
2025-05-21 18:46:07 -0700

    replaced mr young vs mr kraus with cheating on a test

--- script.js
+++ script.js
@@ -132,7 +132,7 @@ const questions = [
                         "Have you ever participated in the conga line during a school dance?",
             "Have you ever used the VR headsets in the Fab Lab?",
             "Have you ever joined or viewed the TGS Discord Server?",
-            "Have you seen the Mr. Young vs. Mr. Kraus Video?",
+            "Have you ever intentionally cheated on an test?",
             "Have you seen a Dancing Dads event?",
                         "Have you ever intentionally ghosted another student?",
             "Have you ever intentionally pulled an all nighter? (Grad Night doesn't count)",
